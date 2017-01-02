573660ff59d56697ffb2d0983f645ca406580d51
iliyaM <iliya@way2cu.com>
2017-01-02 17:41:10 +0200

    page control

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -52,14 +52,33 @@ Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
 
-
+	//Menu button and dropDown div
 	Site.menu_desktop = document.querySelector('a.menu');
 	Site.menu_dropdown = document.querySelector('div.drop_down');
 
+	//Click event toggle class visible
 	Site.menu_desktop.addEventListener('click', function(e) {
 		e.preventDefault();
 		Site.menu_dropdown.classList.toggle('visible');
 	});
+
+	//Lightbox
+	Site.gallery = new LightBox('section.gallery div.portfolio a img', true, false, false);
+
+	//Gallery Carousel right
+	Site.gallery_carousel_right = new PageControl('div.container.right', 'div.portfolio');
+		Site.gallery_carousel_right
+		.attachNextControl($('div.container.right a.next'))
+		.attachPreviousControl($('div.container.right a.prev'))
+		.setWrapAround(true);
+
+	//Gallery Carousel Left
+	Site.gallery_carousel_left = new PageControl('div.container.left', 'div.portfolio');
+		Site.gallery_carousel_left
+		.attachNextControl($('div.container.left a.next'))
+		.attachPreviousControl($('div.container.left a.prev'))
+		.setWrapAround(true);
+
 
 
 
